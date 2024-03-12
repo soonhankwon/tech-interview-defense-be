d5b14c350aac796a6dcb2c9c3c32f4721334b3c7
soonhankwon <soonable@naver.com>
2024-03-12 21:54:53 +0900

    feat: 유저 레포지토리

---
+++ src/main/java/dev/techmentordefensebe/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dev.techmentordefensebe.user.repository;
+
+import dev.techmentordefensebe.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
