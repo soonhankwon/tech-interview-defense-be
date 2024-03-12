45006e81d50190c51c31a00900d6cd94cfb90219
soonhankwon <soonable@naver.com>
2024-03-12 18:54:09 +0900

    Initial commit

---
+++ src/main/java/dev/techmentordefensebe/TechMentorDefenseBeApplication.java
@@ -0,0 +1,13 @@
+package dev.techmentordefensebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechMentorDefenseBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechMentorDefenseBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/techmentordefensebe/TechMentorDefenseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.techmentordefensebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechMentorDefenseBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
