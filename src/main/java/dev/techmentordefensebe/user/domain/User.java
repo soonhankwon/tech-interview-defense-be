fa6a11b5431bc4ae0fc37bdb53db5b9685268002
soonhankwon <soonable@naver.com>
2024-03-12 21:54:35 +0900

    feat: 유저 도메인 Entity 정의

---
+++ src/main/java/dev/techmentordefensebe/user/domain/User.java
@@ -0,0 +1,49 @@
+package dev.techmentordefensebe.user.domain;
+
+import dev.techmentordefensebe.user.enumtype.OauthProvider;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "`user`")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "oauth_login_type", nullable = false)
+    private OauthProvider oauthLoginType;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at", nullable = false)
+    private LocalDateTime modifiedAt;
+}
