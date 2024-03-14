3c464670079df1e759a087065ccb95531a450e5d
soonhankwon <soonable@naver.com>
2024-03-14 16:23:44 +0900

    feat: 로그인 필요 예외코드 추가(#3) - 인증예외

--- src/main/java/dev/techmentordefensebe/common/enumtype/ErrorCode.java
+++ src/main/java/dev/techmentordefensebe/common/enumtype/ErrorCode.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-
+    INTERNAL_SERVER_ERROR(5000, "서버내부 오류입니다."),
     NOT_EXISTS_USER(4000, "유저가 존재하지 않습니다."),
-    INTERNAL_SERVER_ERROR(5000, "서버내부 오류입니다.");
+    LOGIN_REQUIRED(4001, "로그인이 필요합니다.");
 
     private final int code;
     private final String message;
