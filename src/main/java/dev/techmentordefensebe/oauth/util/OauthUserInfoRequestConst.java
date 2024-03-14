cd0cf3455c72b7f42e6cda13362889f21091dee0
soonhankwon <soonable@naver.com>
2024-03-14 14:33:44 +0900

    feat: 프로필이미지URL 조회 상수 추가 - Oauth 프로필정보 조회 spec 변수

--- src/main/java/dev/techmentordefensebe/oauth/util/OauthUserInfoRequestConst.java
+++ src/main/java/dev/techmentordefensebe/oauth/util/OauthUserInfoRequestConst.java
@@ -7,5 +7,7 @@ public final class OauthUserInfoRequestConst {
     public static final String PROFILE = "profile";
     public static final String KAKAO_ACCOUNT = "kakao_account";
     public static final String KAKAO_NICKNAME = "nickname";
+    public static final String KAKAO_PROFILE_IMG_URL = "profile_image_url";
+    public static final String GOOGLE_PROFILE_IMG_URL = "picture";
     public static final String GOOGLE_NICKNAME = "name";
 }
