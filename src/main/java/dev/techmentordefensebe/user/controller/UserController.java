5da9a7670662ebc2e92a8d21cdf2da879428667a
soonhankwon <soonable@naver.com>
2024-03-14 14:31:33 +0900

    feat: UserController - user 가입 API

---
+++ src/main/java/dev/techmentordefensebe/user/controller/UserController.java
@@ -0,0 +1,26 @@
+package dev.techmentordefensebe.user.controller;
+
+import dev.techmentordefensebe.user.dto.UserAddRequest;
+import dev.techmentordefensebe.user.dto.UserAddResponse;
+import dev.techmentordefensebe.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserAddResponse> addUser(@RequestBody UserAddRequest request) {
+        UserAddResponse res = userService.addUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+}
