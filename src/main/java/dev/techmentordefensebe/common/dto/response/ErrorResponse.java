320efa1f914ae92ddce8fbc4470f5a8f8c3c916e
soonhankwon <soonable@naver.com>
2024-03-14 16:13:05 +0900

    feat: ErrorResponse Record(#3) - 에러 Response dto - Unit Test: toResponseEntity

---
+++ src/main/java/dev/techmentordefensebe/common/dto/response/ErrorResponse.java
@@ -0,0 +1,20 @@
+package dev.techmentordefensebe.common.dto.response;
+
+import dev.techmentordefensebe.common.enumtype.ErrorCode;
+import dev.techmentordefensebe.common.exception.CustomException;
+import org.springframework.http.ResponseEntity;
+
+public record ErrorResponse(
+        int code,
+        String message
+) {
+    public static ResponseEntity<ErrorResponse> toResponseEntity(CustomException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+        return ResponseEntity.status(exception.getStatus())
+                .body(
+                        new ErrorResponse(
+                                errorCode.getCode(),
+                                errorCode.getMessage())
+                );
+    }
+}
