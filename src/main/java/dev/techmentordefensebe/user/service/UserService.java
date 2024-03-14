c9a1b84225f68fdcbbe0a2067e318c4b39d17484
soonhankwon <soonable@naver.com>
2024-03-14 14:31:55 +0900

    feat: UserService - user 가입 비즈니스 로직

--- src/main/java/dev/techmentordefensebe/user/service/UserService.java
+++ src/main/java/dev/techmentordefensebe/user/service/UserService.java
@@ -1,12 +1,27 @@
 package dev.techmentordefensebe.user.service;
 
+import dev.techmentordefensebe.oauth.enumtype.OauthProvider;
+import dev.techmentordefensebe.user.domain.User;
+import dev.techmentordefensebe.user.dto.UserAddRequest;
+import dev.techmentordefensebe.user.dto.UserAddResponse;
 import dev.techmentordefensebe.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public UserAddResponse addUser(UserAddRequest request) {
+        if (request.oauthLoginType() == OauthProvider.NONE) {
+            //TODO email 인증 로직
+        }
+        User user = request.toEntity();
+        userRepository.save(user);
+        return UserAddResponse.from(user);
+    }
 }
