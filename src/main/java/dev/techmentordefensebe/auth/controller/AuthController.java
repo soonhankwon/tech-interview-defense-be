c38fb1c1735a00d177c3f9f6fd53a43ee6193906
soonhankwon <soonable@naver.com>
2024-03-14 14:20:20 +0900

    feat: AuthController - 인증 API(로그인)

---
+++ src/main/java/dev/techmentordefensebe/auth/controller/AuthController.java
@@ -0,0 +1,21 @@
+package dev.techmentordefensebe.auth.controller;
+
+import dev.techmentordefensebe.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> emailLogin() {
+        return authService.login();
+    }
+}
