284467a926f361581e00df8982e0c0718732593a
soonhankwon <soonable@naver.com>
2024-03-15 19:34:19 +0900

    feat: 쿼리 수정(#4) - 유저테이블 조인후 메서드 파라미터가 and 조건에 맞는 Chat 조회

--- src/main/java/dev/techmentordefensebe/chat/repository/ChatRepository.java
+++ src/main/java/dev/techmentordefensebe/chat/repository/ChatRepository.java
@@ -3,14 +3,13 @@ package dev.techmentordefensebe.chat.repository;
 import dev.techmentordefensebe.chat.domain.Chat;
 import dev.techmentordefensebe.user.domain.User;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
     List<Chat> findAllByUser(User user);
 
-    @Query("select distinct c from Chat c join fetch ChatMessage m on m.chat = c where :chatId = c.id")
-    Chat findChatByChatId(Long chatId);
+    Optional<Chat> findByIdAndUser_Email(Long chatId, String email);
 }
