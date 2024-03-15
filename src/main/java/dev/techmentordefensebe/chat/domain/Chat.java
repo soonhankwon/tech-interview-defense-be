d0f0bcd52af3365edb7a026a471ea085f05604c1
soonhankwon <soonable@naver.com>
2024-03-15 18:08:42 +0900

    feat: Chat 엔티티 정의 - ChatMentor: Embedded(임베디드 타입)

---
+++ src/main/java/dev/techmentordefensebe/chat/domain/Chat.java
@@ -0,0 +1,56 @@
+package dev.techmentordefensebe.chat.domain;
+
+import dev.techmentordefensebe.common.domain.BaseTimeEntity;
+import dev.techmentordefensebe.tech.domain.Tech;
+import dev.techmentordefensebe.user.domain.User;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "`chat`")
+public class Chat extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @JoinColumn(name = "tech_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Tech tech;
+
+    @Embedded
+    private ChatMentor chatMentor;
+
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<ChatMessage> chatMessages = new ArrayList<>();
+
+    private Chat(User user, Tech tech, ChatMentor chatMentor) {
+        this.user = user;
+        this.tech = tech;
+        this.chatMentor = chatMentor;
+    }
+
+    public static Chat of(User user, Tech tech, ChatMentor chatMentor) {
+        return new Chat(user, tech, chatMentor);
+    }
+}
