957ae0bb85bd3674d694d426c94a9071544f6501
soonhankwon <soonable@naver.com>
2024-03-15 19:39:03 +0900

    feat: 예외코드 추가(#4) - 4001, 4003, 4004

--- src/main/java/dev/techmentordefensebe/common/enumtype/ErrorCode.java
+++ src/main/java/dev/techmentordefensebe/common/enumtype/ErrorCode.java
@@ -8,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(5000, "서버내부 오류입니다."),
     NOT_EXISTS_USER(4000, "유저가 존재하지 않습니다."),
-    LOGIN_REQUIRED(4001, "로그인이 필요합니다.");
+    NOT_EXISTS_USER_EMAIL(4001, "유저 이메일이 존재하지 않습니다."),
+    LOGIN_REQUIRED(4002, "로그인이 필요합니다."),
+    NOT_EXISTS_TOPIC_NAME(4003, "토픽 이름이 존재하지 않습니다."),
+    NOT_EXISTS_CHAT_ID(4004, "채팅 ID가 존재하지 않습니다.");
 
     private final int code;
     private final String message;
