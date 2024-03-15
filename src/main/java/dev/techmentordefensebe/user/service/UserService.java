865e5c3e61c9d5188092e0c26cbc87bd35db169f
soonhankwon <soonable@naver.com>
2024-03-15 20:52:24 +0900

    feat: 유저기술 추가 비즈니스 로직 - 기술중복체크포함

--- src/main/java/dev/techmentordefensebe/user/service/UserService.java
+++ src/main/java/dev/techmentordefensebe/user/service/UserService.java
@@ -1,11 +1,22 @@
 package dev.techmentordefensebe.user.service;
 
+import dev.techmentordefensebe.common.enumtype.ErrorCode;
+import dev.techmentordefensebe.common.exception.CustomException;
+import dev.techmentordefensebe.common.security.impl.UserDetailsImpl;
 import dev.techmentordefensebe.oauth.enumtype.OauthProvider;
+import dev.techmentordefensebe.tech.domain.Tech;
+import dev.techmentordefensebe.tech.repository.TechRepository;
 import dev.techmentordefensebe.user.domain.User;
+import dev.techmentordefensebe.user.domain.UserTech;
+import dev.techmentordefensebe.user.dto.UserTechDTO;
 import dev.techmentordefensebe.user.dto.request.UserAddRequest;
+import dev.techmentordefensebe.user.dto.request.UserTechAddRequest;
 import dev.techmentordefensebe.user.dto.response.UserAddResponse;
+import dev.techmentordefensebe.user.dto.response.UserTechAddResponse;
 import dev.techmentordefensebe.user.repository.UserRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final TechRepository techRepository;
 
     @Transactional
     public UserAddResponse addUser(UserAddRequest request) {
@@ -23,5 +35,31 @@ public class UserService {
         User user = request.toEntity();
         userRepository.save(user);
         return UserAddResponse.from(user);
+    }
+
+    @Transactional
+    public UserTechAddResponse addUserTech(UserDetailsImpl userDetails, UserTechAddRequest request) {
+        String email = userDetails.getEmail();
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(HttpStatus.BAD_REQUEST, ErrorCode.NOT_EXISTS_USER_EMAIL));
+
+        Tech tech = techRepository.findByName(request.techName())
+                .orElseThrow(() -> new CustomException(HttpStatus.BAD_REQUEST, ErrorCode.NOT_EXISTS_TECH_NAME));
+
+        UserTech userTech = request.toEntity(user, tech);
+        List<UserTech> userTechs = user.getUserTechs();
+
+        //DB 쿼리로 중복체크를 하는대신 메모리에 올려서 중복체크
+        boolean isAnyMatchRequestTechName = userTechs.stream()
+                .map(ut -> ut.getTech().getName())
+                .anyMatch(name -> tech.getName().equals(name));
+
+        if (isAnyMatchRequestTechName) {
+            throw new CustomException(HttpStatus.BAD_REQUEST, ErrorCode.EXISTS_DUPLICATED_TECH_NAME);
+        }
+        userTechs.add(userTech);
+        List<UserTechDTO> userTechDTOS = UserTechDTO.from(userTechs);
+        return UserTechAddResponse.from(userTechDTOS);
     }
 }
