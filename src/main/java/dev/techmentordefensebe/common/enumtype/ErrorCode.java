e8e3246be93870a796113bc710d86dbd4fb345be
soonhankwon <soonable@naver.com>
2024-03-15 20:44:03 +0900

    feat: ErrorCode 추가 - 4003, 4005

--- src/main/java/dev/techmentordefensebe/common/enumtype/ErrorCode.java
+++ src/main/java/dev/techmentordefensebe/common/enumtype/ErrorCode.java
@@ -10,8 +10,9 @@ public enum ErrorCode {
     NOT_EXISTS_USER(4000, "유저가 존재하지 않습니다."),
     NOT_EXISTS_USER_EMAIL(4001, "유저 이메일이 존재하지 않습니다."),
     LOGIN_REQUIRED(4002, "로그인이 필요합니다."),
-    NOT_EXISTS_TOPIC_NAME(4003, "토픽 이름이 존재하지 않습니다."),
-    NOT_EXISTS_CHAT_ID(4004, "채팅 ID가 존재하지 않습니다.");
+    NOT_EXISTS_TECH_NAME(4003, "기술 이름이 존재하지 않습니다."),
+    NOT_EXISTS_CHAT_ID(4004, "채팅 ID가 존재하지 않습니다."),
+    EXISTS_DUPLICATED_TECH_NAME(4005, "중복된 기술 이름이 존재합니다.");
 
     private final int code;
     private final String message;
