162fd20160b8900a46dba886d7fca23c6c69d4be
soonhankwon <soonable@naver.com>
2024-03-15 18:15:32 +0900

    feat: email 필드로 사용자 식별 - oauthProviderUniqueKey: 로그인시 매번 바뀌는것 확인

--- src/main/java/dev/techmentordefensebe/user/repository/UserRepository.java
+++ src/main/java/dev/techmentordefensebe/user/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package dev.techmentordefensebe.user.repository;
 
 import dev.techmentordefensebe.user.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByOauthProviderUniqueKey(String oauthProviderUniqueKey);
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
