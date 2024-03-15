0bdce72554525ba4be9d5e76bbe4a0205c97ce71
soonhankwon <soonable@naver.com>
2024-03-15 16:10:25 +0900

    feat: JpaAuditing 기능 사용 - BaseTimeEntity(createdAt, modifiedAt) - CreatedTimeEntity(createdAt) - JpaAuditingConfig(설정)

---
+++ src/main/java/dev/techmentordefensebe/common/domain/CreatedTimeEntity.java
@@ -0,0 +1,19 @@
+package dev.techmentordefensebe.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = AuditingEntityListener.class)
+public abstract class CreatedTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
