52b521b0649f6cdcb454cad3293603da019e8698
soonhankwon <soonable@naver.com>
2024-03-15 19:39:42 +0900

    feat: 유저기술 엔티티 정의(#4) - User, Tech 브릿지 테이블

---
+++ src/main/java/dev/techmentordefensebe/user/domain/UserTech.java
@@ -0,0 +1,35 @@
+package dev.techmentordefensebe.user.domain;
+
+
+import dev.techmentordefensebe.common.domain.CreatedTimeEntity;
+import dev.techmentordefensebe.tech.domain.Tech;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "`user_tech`")
+public class UserTech extends CreatedTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tech_id", nullable = false)
+    private Tech tech;
+}
