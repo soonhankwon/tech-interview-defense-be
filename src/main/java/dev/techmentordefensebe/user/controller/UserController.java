dd3bf3e7afa57222891623e3a91b5849f56e5c14
soonhankwon <soonable@naver.com>
2024-03-15 20:52:47 +0900

    feat: 유저기술 추가 API - POST

--- src/main/java/dev/techmentordefensebe/user/controller/UserController.java
+++ src/main/java/dev/techmentordefensebe/user/controller/UserController.java
@@ -1,11 +1,15 @@
 package dev.techmentordefensebe.user.controller;
 
+import dev.techmentordefensebe.common.security.impl.UserDetailsImpl;
 import dev.techmentordefensebe.user.dto.request.UserAddRequest;
+import dev.techmentordefensebe.user.dto.request.UserTechAddRequest;
 import dev.techmentordefensebe.user.dto.response.UserAddResponse;
+import dev.techmentordefensebe.user.dto.response.UserTechAddResponse;
 import dev.techmentordefensebe.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +25,13 @@ public class UserController {
     @PostMapping
     public ResponseEntity<UserAddResponse> addUser(@RequestBody UserAddRequest request) {
         UserAddResponse res = userService.addUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @PostMapping("/techs")
+    public ResponseEntity<UserTechAddResponse> addUserTech(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                           @RequestBody UserTechAddRequest request) {
+        UserTechAddResponse res = userService.addUserTech(userDetails, request);
         return ResponseEntity.status(HttpStatus.CREATED).body(res);
     }
 }
