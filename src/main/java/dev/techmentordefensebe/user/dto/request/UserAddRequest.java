261d959686a1d7860e3970436bbb986f1b967a31
soonhankwon <soonable@naver.com>
2024-03-16 16:05:24 +0900

    feat: 유저객체 생성책임을 User로 SRP

--- src/main/java/dev/techmentordefensebe/user/dto/request/UserAddRequest.java
+++ src/main/java/dev/techmentordefensebe/user/dto/request/UserAddRequest.java
@@ -1,7 +1,6 @@
 package dev.techmentordefensebe.user.dto.request;
 
 import dev.techmentordefensebe.oauth.enumtype.OauthProvider;
-import dev.techmentordefensebe.user.domain.User;
 
 //TODO 멤버변수 Validation 필요
 public record UserAddRequest(
@@ -10,7 +9,4 @@ public record UserAddRequest(
         OauthProvider oauthLoginType,
         String profileImgUrl
 ) {
-    public User toEntity() {
-        return new User(this);
-    }
 }
