c789453c91cce12b299e8793b35500209a6499fd
soonhankwon <soonable@naver.com>
2024-03-18 21:26:33 +0900

    feat: CustomAuthenticationEntryPoint에 handlerExceptionResolver 적용 - 불필요 메서드 모두 삭제

--- src/main/java/dev/techmentordefensebe/common/security/handler/CustomAuthenticationEntryPoint.java
+++ src/main/java/dev/techmentordefensebe/common/security/handler/CustomAuthenticationEntryPoint.java
@@ -1,57 +1,26 @@
 package dev.techmentordefensebe.common.security.handler;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.techmentordefensebe.common.dto.response.ErrorResponse;
-import dev.techmentordefensebe.common.enumtype.ErrorCode;
-import dev.techmentordefensebe.common.exception.CustomException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 @Component
-@RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private final ObjectMapper objectMapper;
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    public CustomAuthenticationEntryPoint(
+            @Qualifier("handlerExceptionResolver") HandlerExceptionResolver handlerExceptionResolver) {
+        this.handlerExceptionResolver = handlerExceptionResolver;
+    }
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException {
-        Object e = request.getAttribute("exception");
-        if (!isCustomException(e)) {
-            responseWithJson(response, new CustomException(HttpStatus.BAD_REQUEST, ErrorCode.LOGIN_REQUIRED));
-            return;
-        }
-        CustomException customException = convertToCustomException(e);
-        responseWithJson(response, customException);
-    }
-
-    private boolean isCustomException(Object ex) {
-        return ex instanceof CustomException;
-    }
-
-    private CustomException convertToCustomException(Object object) {
-        return (CustomException) object;
-    }
-
-    private void responseWithJson(HttpServletResponse response, CustomException customException) throws IOException {
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.addHeader(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
-        response.setStatus(customException.getStatus().value());
-        response.getWriter().write(toJsonResponse(customException));
-    }
-
-    private String toJsonResponse(CustomException customException)
-            throws JsonProcessingException {
-        ErrorResponse response = ErrorResponse.from(customException);
-        return objectMapper.writeValueAsString(response);
+                         AuthenticationException authException) {
+        handlerExceptionResolver.resolveException(request, response, null, authException);
     }
 }
