7a8c0185908e0e7177f16b94eab4b279dd4ff69b
soonhankwon <soonable@naver.com>
2024-03-18 21:28:47 +0900

    feat: Pagination 쿼리 추가

--- src/main/java/dev/techmentordefensebe/chat/repository/ChatRepository.java
+++ src/main/java/dev/techmentordefensebe/chat/repository/ChatRepository.java
@@ -2,14 +2,17 @@ package dev.techmentordefensebe.chat.repository;
 
 import dev.techmentordefensebe.chat.domain.Chat;
 import dev.techmentordefensebe.user.domain.User;
-import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    List<Chat> findAllByUser(User user);
+    Page<Chat> findAllByUser(User user, Pageable pageable);
+
+    Page<Chat> findAllByUserAndIsDefenseMode(User user, Boolean isDefenseMode, Pageable pageable);
 
     Optional<Chat> findByIdAndUser_Email(Long chatId, String email);
 }
