a568f681faf3a4f3c02049833867c137c0e6f8ef
soonhankwon <soonable@naver.com>
2024-03-18 21:21:57 +0900

    test: 메서드삭제로 불필요 테스트 삭제

--- src/test/java/dev/techmentordefensebe/common/dto/response/ErrorResponseTest.java
+++ src/test/java/dev/techmentordefensebe/common/dto/response/ErrorResponseTest.java
@@ -3,23 +3,9 @@ package dev.techmentordefensebe.common.dto.response;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import dev.techmentordefensebe.common.enumtype.ErrorCode;
-import dev.techmentordefensebe.common.exception.CustomException;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 class ErrorResponseTest {
-
-    @Test
-    void toResponseEntity() {
-        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
-        ErrorCode errorCode = ErrorCode.NOT_EXISTS_USER;
-        CustomException customException = new CustomException(badRequest, errorCode);
-
-        ResponseEntity<ErrorResponse> responseEntity = ErrorResponse.toResponseEntity(customException);
-
-        assertThat(responseEntity.getStatusCode()).isSameAs(badRequest);
-    }
 
     @Test
     void code() {
