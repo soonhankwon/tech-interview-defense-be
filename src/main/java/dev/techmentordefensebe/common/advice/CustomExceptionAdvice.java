2b448c2586da26821bcca94727c533970aaeaeb5
soonhankwon <soonable@naver.com>
2024-03-18 21:25:50 +0900

    feat: ExceptionHandler 추가 및 toResponseEntity 메서드 삭제

--- src/main/java/dev/techmentordefensebe/common/advice/CustomExceptionAdvice.java
+++ src/main/java/dev/techmentordefensebe/common/advice/CustomExceptionAdvice.java
@@ -4,16 +4,18 @@ import dev.techmentordefensebe.common.dto.response.ErrorResponse;
 import dev.techmentordefensebe.common.enumtype.ErrorCode;
 import dev.techmentordefensebe.common.exception.CustomException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @RestControllerAdvice
 public class CustomExceptionAdvice {
 
     @ExceptionHandler(CustomException.class)
-    protected ResponseEntity<?> handleCustomException(CustomException exception) {
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException exception) {
         ErrorCode errorCode = exception.getErrorCode();
         log.warn(
                 String.format(
@@ -21,6 +23,20 @@ public class CustomExceptionAdvice {
                         exception.getStatus().value(), errorCode.getCode(), errorCode.getMessage()
                 )
         );
-        return ErrorResponse.toResponseEntity(exception);
+        ErrorResponse res = ErrorResponse.from(exception);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgTypeMismatchException(
+            MethodArgumentTypeMismatchException exception) {
+        ErrorResponse res = ErrorResponse.from(exception);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        ErrorResponse res = ErrorResponse.from(exception);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
     }
 }
