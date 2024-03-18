385d4b7c0955618cdc6c202712ee43596b7f7ddb
soonhankwon <soonable@naver.com>
2024-03-18 21:29:46 +0900

    feat: 유저의 채팅목록(멘토링 및 디펜스)조회 비즈니스 로직구현 - 페이지네이션 적용 - mode param 에 따른 분기로직처리

--- src/main/java/dev/techmentordefensebe/chat/service/ChatService.java
+++ src/main/java/dev/techmentordefensebe/chat/service/ChatService.java
@@ -2,12 +2,13 @@ package dev.techmentordefensebe.chat.service;
 
 import dev.techmentordefensebe.chat.domain.Chat;
 import dev.techmentordefensebe.chat.domain.ChatMentor;
+import dev.techmentordefensebe.chat.dto.ChatDTO;
 import dev.techmentordefensebe.chat.dto.ChatMessageDTO;
 import dev.techmentordefensebe.chat.dto.request.ChatAddRequest;
 import dev.techmentordefensebe.chat.dto.response.ChatAddResponse;
 import dev.techmentordefensebe.chat.dto.response.ChatDeleteResponse;
 import dev.techmentordefensebe.chat.dto.response.ChatDetailsGetResponse;
-import dev.techmentordefensebe.chat.dto.response.ChatGetResponse;
+import dev.techmentordefensebe.chat.dto.response.ChatsGetResponse;
 import dev.techmentordefensebe.chat.repository.ChatRepository;
 import dev.techmentordefensebe.common.enumtype.ErrorCode;
 import dev.techmentordefensebe.common.exception.CustomException;
@@ -19,6 +20,8 @@ import dev.techmentordefensebe.user.repository.UserRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +29,9 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class ChatService {
+
+    private static final int PAGE_SIZE = 10;
+    private static final String DEFENSE_MODE = "defense";
 
     private final ChatRepository chatRepository;
     private final UserRepository userRepository;
@@ -44,24 +50,38 @@ public class ChatService {
                 .orElseThrow(() -> new CustomException(HttpStatus.BAD_REQUEST, ErrorCode.NOT_EXISTS_TECH_NAME));
 
         ChatMentor chatMentor = ChatMentor.from(request);
-        Chat chat = Chat.of(user, tech, chatMentor);
+        Chat chat = Chat.of(user, tech, chatMentor, request.isDefenseMode());
         chatRepository.save(chat);
 
         return ChatAddResponse.of(user, tech, chat);
     }
 
-    public List<ChatGetResponse> findChatsByUser(UserDetailsImpl userDetails) {
+    public ChatsGetResponse findChatsByUser(UserDetailsImpl userDetails, int pageNumber, String mode) {
         String email = userDetails.getEmail();
-        assert email != null;
-
         User user = userRepository
                 .findByEmail(email)
                 .orElseThrow(() -> new CustomException(HttpStatus.BAD_REQUEST, ErrorCode.NOT_EXISTS_USER_EMAIL));
 
-        //TODO Paging 필요
-        List<Chat> chats = chatRepository.findAllByUser(user);
-        return chats.stream()
-                .map(ChatGetResponse::from)
+        PageRequest pageRequest = PageRequest.of(pageNumber, PAGE_SIZE);
+        //TODO mode validation 필요
+        if (mode != null) {
+            boolean isDefenseMode = isModeDefense(mode);
+            Page<Chat> page = chatRepository.findAllByUserAndIsDefenseMode(user, isDefenseMode, pageRequest);
+            List<ChatDTO> chatDTOS = convertPageChatDTOS(page);
+            return ChatsGetResponse.of(page.getTotalPages(), chatDTOS);
+        }
+        Page<Chat> page = chatRepository.findAllByUser(user, pageRequest);
+        List<ChatDTO> chatDTOS = convertPageChatDTOS(page);
+        return ChatsGetResponse.of(page.getTotalPages(), chatDTOS);
+    }
+
+    private boolean isModeDefense(String mode) {
+        return mode.equals(DEFENSE_MODE);
+    }
+
+    private List<ChatDTO> convertPageChatDTOS(Page<Chat> page) {
+        return page.stream()
+                .map(ChatDTO::from)
                 .collect(Collectors.toList());
     }
 
