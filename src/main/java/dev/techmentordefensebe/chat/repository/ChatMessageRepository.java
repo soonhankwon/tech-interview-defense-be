e087785f1fa677d92c9393836d161eb0bfd63e96
soonhankwon <soonable@naver.com>
2024-03-20 17:42:27 +0900

    feat: findAllByChat - Chat 메세지목록 리턴

--- src/main/java/dev/techmentordefensebe/chat/repository/ChatMessageRepository.java
+++ src/main/java/dev/techmentordefensebe/chat/repository/ChatMessageRepository.java
@@ -1,9 +1,12 @@
 package dev.techmentordefensebe.chat.repository;
 
+import dev.techmentordefensebe.chat.domain.Chat;
 import dev.techmentordefensebe.chat.domain.ChatMessage;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findAllByChat(Chat chat);
 }
