3655970c1e0cf582844ce3c8bdf6e46e8370a4ae
soonhankwon <soonable@naver.com>
2024-03-20 17:43:46 +0900

    feat: PromptGenerator - 프롬프트 상수(String) 클래스

---
+++ src/main/java/dev/techmentordefensebe/openai/util/PromptGenerator.java
@@ -0,0 +1,7 @@
+package dev.techmentordefensebe.openai.util;
+
+public class PromptGenerator {
+    public static final String HI = "안녕하세요. ";
+    public static final String INTRODUCE = "멘토입니다.";
+    public static final String WHAT_HELP = "무엇을 도와드릴까요?";
+}
