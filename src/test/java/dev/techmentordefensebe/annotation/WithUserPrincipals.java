3aecd3d4c16f34a9eeabf788df5c5367f01174c6
soonhankwon <soonable@naver.com>
2024-03-25 18:39:06 +0900

    feat: WithUserPrincipals(for test) - Test시 유저권한적용

---
+++ src/test/java/dev/techmentordefensebe/annotation/WithUserPrincipals.java
@@ -0,0 +1,13 @@
+package dev.techmentordefensebe.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithUserPrincipalsSecurityContextFactory.class)
+public @interface WithUserPrincipals {
+    String id() default "1";
+
+    String email() default "test@gmail.com";
+}
