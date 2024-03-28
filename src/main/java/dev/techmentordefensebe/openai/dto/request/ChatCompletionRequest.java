50f027a28fbb14186f24676531f31b41c4fd7bab
soonhankwon <soonable@naver.com>
2024-03-28 16:21:36 +0900

    feat: isDefense 매개변수에 따라 system 프롬프트 생성로직 분기추가

--- src/main/java/dev/techmentordefensebe/openai/dto/request/ChatCompletionRequest.java
+++ src/main/java/dev/techmentordefensebe/openai/dto/request/ChatCompletionRequest.java
@@ -2,6 +2,7 @@ package dev.techmentordefensebe.openai.dto.request;
 
 import dev.techmentordefensebe.chat.domain.Chat;
 import dev.techmentordefensebe.openai.dto.ChatCompletionMessageDTO;
+import dev.techmentordefensebe.openai.util.PromptGenerator;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,12 +10,13 @@ public record ChatCompletionRequest(
         String model,
         List<ChatCompletionMessageDTO> messages
 ) {
-    public static ChatCompletionRequest ofDefaultSetting(String model, Chat chat) {
+    public static ChatCompletionRequest ofDefaultSetting(String model, Chat chat, boolean isDefenseMode) {
         String mentorTopic = chat.getTech().getName();
         String mentorTone = chat.getChatMentor().getTone();
-        String systemPrompt = "당신은 " + mentorTopic + "전문가입니다." + mentorTone + "하게 답변해주세요.";
-        ChatCompletionMessageDTO system = ChatCompletionMessageDTO.of("system", systemPrompt);
+        // isDefenseMode 여부에 따라 프롬프트가 많이 달라짐
+        String systemPrompt = PromptGenerator.getMentorSystemPrompt(mentorTopic, mentorTone, isDefenseMode);
 
+        ChatCompletionMessageDTO system = ChatCompletionMessageDTO.of("system", systemPrompt);
         List<ChatCompletionMessageDTO> messages = new ArrayList<>();
         messages.add(system);
 
