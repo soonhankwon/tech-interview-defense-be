c7827714e172929712e16c905ad6cfccfad3512a
soonhankwon <soonable@naver.com>
2024-03-28 16:28:14 +0900

    test: validateProvider exception 테스트

--- src/test/java/dev/techmentordefensebe/oauth/enumtype/OauthProviderTest.java
+++ src/test/java/dev/techmentordefensebe/oauth/enumtype/OauthProviderTest.java
@@ -1,24 +1,27 @@
 package dev.techmentordefensebe.oauth.enumtype;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
 class OauthProviderTest {
 
     @Test
-    void validateProvider() {
+    void validateProvider_wrong_provider_exception() {
+        assertThatThrownBy(() -> OauthProvider.validateProvider("wrong"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void getName() {
-        assertThat(OauthProvider.KAKAO.getName()).isEqualTo("kakao");
+    void getValue() {
+        assertThat(OauthProvider.KAKAO.getValue()).isEqualTo("kakao");
     }
 
     @Test
     void values() {
         OauthProvider[] values = OauthProvider.values();
-        assertThat(values.length).isEqualTo(2);
+        assertThat(values.length).isEqualTo(3);
     }
 
     @Test
