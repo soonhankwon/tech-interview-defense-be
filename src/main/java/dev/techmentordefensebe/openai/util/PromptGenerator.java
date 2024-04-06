f2b16a04bb4dabe87b2f5ac4157fe4e3e78e9863
soonhankwon <soonable@naver.com>
2024-04-06 19:28:54 +0900

    refactor: PromptGenerator prompt 조합(상수 선언 및 프롬프트 생성)

--- src/main/java/dev/techmentordefensebe/openai/util/PromptGenerator.java
+++ src/main/java/dev/techmentordefensebe/openai/util/PromptGenerator.java
@@ -1,21 +1,38 @@
 package dev.techmentordefensebe.openai.util;
 
+import dev.techmentordefensebe.tech.domain.Tech;
+
 public class PromptGenerator {
-    public static final String HI = "안녕하세요. ";
-    public static final String INTRODUCE = "멘토입니다.";
-    public static final String WHAT_HELP = "무엇을 도와드릴까요?";
+    private static final String HI = "안녕하세요. ";
+    private static final String MENTOR = "멘토입니다.";
+    private static final String PROFESSIONAL = "전문가입니다.";
+    private static final String INTERVIEWER = "기술 면접관입니다.";
+    private static final String WHAT_HELP = "무엇을 도와드릴까요?";
+    private static final String START_INTERVIEW = "면접을 시작하겠습니다.";
+    private static final String YOU = "당신은 ";
+    private static final String INTERVIEWER_RULE_1 = "면접상황과 맞지 않는 말은 하지 말아주세요.";
+    private static final String INTERVIEWER_RULE_2 = "질문은 꼭 1개만 해주세요.";
+    private static final String INTERVIEWER_RULE_3 = "상대방의 답변을 받으면 꼭 다음 질문을 해서 인터뷰를 이어나가주세요.";
+    private static final String INTERVIEWER_RULE_4 = "상대방의 답변의 정확성이 부정확하다면 꼭 추가적인 질문을 해주세요.";
+
+    public static String welcomePrompt(Tech tech, boolean isDefenseMode) {
+        if (isDefenseMode) {
+            return HI + tech.getName() + INTERVIEWER + START_INTERVIEW;
+        }
+        return HI + tech.getName() + MENTOR + WHAT_HELP;
+    }
 
     public static String getMentorSystemPrompt(final String mentorTopic,
                                                final String mentorTone,
                                                final boolean isDefenseMode) {
         if (isDefenseMode) {
-            return "당신은 " + mentorTopic + "전문가입니다." + mentorTone + "하게 답변해주세요.";
+            return YOU + mentorTopic + PROFESSIONAL + mentorTone + "하게 답변해주세요.";
         }
-        return "당신은 " + mentorTopic + " 기술 면접관입니다. 그리고 현재 면접자를 인터뷰하는 상황입니다."
+        return YOU + mentorTopic + INTERVIEWER + "현재 면접자를 기술 면접하는 상황입니다."
                 + mentorTone + "하게 질문해주세요." +
-                "면접상황과 맞지 않는 말은 하지 말아주세요." +
-                "질문은 꼭 1개만 해주세요." +
-                "상대방의 답변을 받으면 꼭 다음 질문을 해서 인터뷰를 이어나가주세요." +
-                "상대방의 답변의 정확성이 부정확하다면 꼭 추가적인 질문을 해주세요.";
+                INTERVIEWER_RULE_1 +
+                INTERVIEWER_RULE_2 +
+                INTERVIEWER_RULE_3 +
+                INTERVIEWER_RULE_4;
     }
 }
