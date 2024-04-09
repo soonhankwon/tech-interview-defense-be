f45c46c4bc5e0ccb2618a2445546678c81561289
soonhankwon <soonable@naver.com>
2024-04-09 17:05:19 +0900

    feat: API response 유저테크 id 추가

--- src/main/java/dev/techmentordefensebe/user/dto/UserTechDTO.java
+++ src/main/java/dev/techmentordefensebe/user/dto/UserTechDTO.java
@@ -5,12 +5,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public record UserTechDTO(
+        Long userTechId,
         Long techId,
         String techName
 ) {
     public static List<UserTechDTO> from(List<UserTech> userTechs) {
         return userTechs.stream().map(userTech ->
                 new UserTechDTO(
+                        userTech.getId(),
                         userTech.getTech().getId(),
                         userTech.getTech().getName()
                 )
