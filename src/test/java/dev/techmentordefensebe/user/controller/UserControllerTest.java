2844300186ffcac3a82a1f799cb89460fb6984a1
soonhankwon <soonable@naver.com>
2024-04-09 17:25:53 +0900

    test: 유저기술 목록조회 & 삭제 WebMvcTest

--- src/test/java/dev/techmentordefensebe/user/controller/UserControllerTest.java
+++ src/test/java/dev/techmentordefensebe/user/controller/UserControllerTest.java
@@ -1,6 +1,7 @@
 package dev.techmentordefensebe.user.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
@@ -16,13 +17,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import dev.techmentordefensebe.annotation.WithUserPrincipals;
 import dev.techmentordefensebe.common.security.impl.UserDetailsImpl;
 import dev.techmentordefensebe.oauth.enumtype.OauthProvider;
+import dev.techmentordefensebe.tech.dto.TechDTO;
 import dev.techmentordefensebe.user.dto.UserTechDTO;
 import dev.techmentordefensebe.user.dto.request.UserAddRequest;
 import dev.techmentordefensebe.user.dto.request.UserTechAddRequest;
 import dev.techmentordefensebe.user.dto.response.UserAddResponse;
 import dev.techmentordefensebe.user.dto.response.UserTechAddResponse;
+import dev.techmentordefensebe.user.dto.response.UserTechDeleteResponse;
+import dev.techmentordefensebe.user.dto.response.UserTechsGetResponse;
 import dev.techmentordefensebe.user.service.UserService;
 import java.util.List;
+import java.util.UUID;
 import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,6 +35,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.MockMvc;
@@ -121,8 +127,62 @@ class UserControllerTest {
     }
 
     private UserTechAddResponse createUserTechAddResponse(String techName) {
-        UserTechDTO existsUserTech = new UserTechDTO(1L, "JAVA");
-        UserTechDTO userTechDTO = new UserTechDTO(2L, techName);
-        return UserTechAddResponse.from(List.of(existsUserTech, userTechDTO));
+        TechDTO existsTechDTO = new TechDTO(1L, "JAVA");
+        TechDTO techDTO = new TechDTO(2L, techName);
+        return UserTechAddResponse.from(List.of(existsTechDTO, techDTO));
+    }
+
+    @DisplayName("[POST]유저기술 목록조회 - 정상호출")
+    @WithUserPrincipals
+    @Test
+    void getUserTechs() throws Exception {
+        when(userService.getUserTechs(any(UserDetailsImpl.class)))
+                .thenReturn(createUserTechsGetResponse());
+
+        mvc.perform(
+                        RestDocumentationRequestBuilders.get("/api/v1/users/techs")
+                                .with(csrf().asHeader())
+                                .header(HttpHeaders.AUTHORIZATION, UUID.randomUUID()))
+                .andDo(print())
+                .andDo(
+                        document(
+                                "get-user-techs",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint())
+                        )
+                )
+                .andExpect(status().isOk());
+    }
+
+    private UserTechsGetResponse createUserTechsGetResponse() {
+        UserTechDTO userTechDTO1 = new UserTechDTO(1L, 1L, "JAVA");
+        UserTechDTO userTechDTO2 = new UserTechDTO(2L, 2L, "SPRING");
+        return UserTechsGetResponse.from(List.of(userTechDTO1, userTechDTO2));
+    }
+
+    @DisplayName("[POST]유저기술 삭제 - 정상호출")
+    @WithUserPrincipals
+    @Test
+    void deleteUserTech() throws Exception {
+        when(userService.deleteUserTech(any(UserDetailsImpl.class), anyLong()))
+                .thenReturn(createUserTechsDeleteResponse());
+
+        mvc.perform(
+                        RestDocumentationRequestBuilders.delete("/api/v1/users/techs/13")
+                                .with(csrf().asHeader())
+                                .header(HttpHeaders.AUTHORIZATION, UUID.randomUUID()))
+                .andDo(print())
+                .andDo(
+                        document(
+                                "delete-user-tech",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint())
+                        )
+                )
+                .andExpect(status().isOk());
+    }
+
+    private UserTechDeleteResponse createUserTechsDeleteResponse() {
+        return UserTechDeleteResponse.from(13L);
     }
 }
