31b6ec3732178a68813eb0cbcf86fdbed43ae7e1
soonhankwon <soonable@naver.com>
2024-04-15 23:33:32 +0900

    feat: isDefenseMode 필드 삭제 - 구현변경

--- src/main/java/dev/techmentordefensebe/chat/dto/ChatDTO.java
+++ src/main/java/dev/techmentordefensebe/chat/dto/ChatDTO.java
@@ -6,14 +6,12 @@ import java.time.LocalDateTime;
 public record ChatDTO(
         Long chatId,
         String mentorTopic,
-        boolean isDefenseMode,
         LocalDateTime createdAt
 ) {
     public static ChatDTO from(Chat chat) {
         return new ChatDTO(
                 chat.getId(),
                 chat.getTech().getName(),
-                chat.getIsDefenseMode(),
                 chat.getCreatedAt()
         );
     }
